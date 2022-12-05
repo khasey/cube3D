50ba8e450228534fe37f8f248f2a48e7a3d503c1
khasey <92310470+khasey@users.noreply.github.com>
2022-12-05 12:31:04 +0100

    Add files via upload

---
+++ cube3d/include/cube3d.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3h.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kthierry <kthierry@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 01:31:17 by kthierry          #+#    #+#             */
+/*   Updated: 2022/11/23 01:36:48 by kthierry         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE3D_H
+# define CUBE3D_H
+
+# define GREEN			"\x1b[32m"
+# define RESET			"\x1b[0m"
+# define RED			"\033[91m"
+
+# include <stdio.h>
+# include <mlx.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+#endif
